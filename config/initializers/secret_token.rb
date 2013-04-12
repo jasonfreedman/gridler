17f616b99e0489041ff7f129eda22c30e31ae4a5
Jason Freedman <jason.freedman@gmail.com>
2013-04-11 18:36:23 -0700

    initla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gridler::Application.config.secret_token = '875dca656641d8dbb7ee36fd26f4224df9a1405c84ddc2f6b06d0115568082188ac59b0785a8326f732955435ebde8f24c2fb84bfc4941519217e305b79b4ee0'
